9593f1cbb8a53c3d28a3048801a45fade4680a8c
Bryanpau <Bryanpau>
2026-09-18 23:57:21 +0100

    project

---
+++ week-4/practice_8/src/main.rs
@@ -0,0 +1,14 @@
+fn main(){[cite: 14]
+
+    //while true[cite: 14]
+
+    let mut x = 0;[cite: 14]
+    loop {[cite: 14]
+        x+=1;[cite: 14]
+        println!("x={}",x);[cite: 14]
+
+        if x==15 {[cite: 14]
+            break;[cite: 14]
+        }[cite: 14]
+    }[cite: 14]
+}[cite: 14]
---
+++ week-4/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\User\Desktop\COS\ 101\b.jbCOS101-main\week-4\practice_8\target\debug\practice_8.exe: C:\Users\User\Desktop\COS\ 101\b.jbCOS101-main\week-4\practice_8\src\main.rs
