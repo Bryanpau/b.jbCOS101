9792668cb55e4e7666e5e4080edec437d5e2d705
Bryanpau <Bryanpau>
2026-09-18 23:59:52 +0100

    project

---
+++ week-4/practice_9/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {[cite: 15]
+
+    let mut count = 0;[cite: 15]
+
+    for num in 1..21 {[cite: 15]
+        if num > 10 {[cite: 15]
+            println!("{:?}",num);[cite: 15]
+            continue;[cite: 15]
+        }[cite: 15]
+        count+=1;[cite: 15]
+    }[cite: 15]
+    println!(" The count of values greater than 10 (between 1 and 20) is: {} ",count);[cite: 15]
+    //outputs 10[cite: 15]
+}[cite: 15]
---
+++ week-4/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\User\Desktop\COS\ 101\b.jbCOS101-main\week-4\practice_9\target\debug\practice_9.exe: C:\Users\User\Desktop\COS\ 101\b.jbCOS101-main\week-4\practice_9\src\main.rs
